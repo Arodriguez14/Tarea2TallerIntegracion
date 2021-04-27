dc15ff6a1f4d0593af46e5ac5ba7745cd228de0d
Andrés Rodríguez <arodriguez14@uc.cl>
2021-04-27 10:44:30 -0300

    cambio link

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ API_URL = 'https://tarea2-arodriguez14.herokuapp.com'
 app = Flask(__name__)
 
 #app.config['SQLALCHEMY_DATABASE_URI']='postgresql://localhost/tarea2'
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://bqdysojcqwhkyn:8fd9468ae25e1a10f5fa49787ef633abb28f01c4cb3facaa6a9de0fb47028830@ec2-54-224-120-186.compute-1.amazonaws.com:5432/davh5fis0le6tg'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://hwxdmseprrvfru:0e35e56ac4b45322e1afc4172e94f476a8947d4c6417eecdf7094c4ec2b5ab58@ec2-54-152-185-191.compute-1.amazonaws.com:5432/dadkecbviif2h3'
+#####app.config['SQLALCHEMY_DATABASE_URI']='postgresql://bqdysojcqwhkyn:8fd9468ae25e1a10f5fa49787ef633abb28f01c4cb3facaa6a9de0fb47028830@ec2-54-224-120-186.compute-1.amazonaws.com:5432/davh5fis0le6tg'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 
